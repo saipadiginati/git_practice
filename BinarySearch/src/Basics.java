07fbe7d84510b2b73769f315d7c4a2a7779b31d5
sai <schaitanya334@gmail.com>
2025-09-18 22:41:04 +0530

    Binary search basics code

---
+++ BinarySearch/src/Basics.java
@@ -0,0 +1,30 @@
+public class Basics {
+
+public void bs(int[] arr,int target){
+
+    int n=arr.length;
+    int low=0;
+    int high=n-1;
+    int count=0;
+    while(low <=high){
+
+       int  mid=(low+high)/2;
+
+       if(arr[mid]==target){
+           System.out.println("given number present at index" + mid);
+           count++;
+           break;
+       }
+        else if(arr[mid] > target) {
+        high=mid-1;
+       }
+        else{
+            low=mid+1;
+       }
+    }
+
+    if(count==0){
+        System.out.println("given number is not present int he array");
+    }
+}
+}
